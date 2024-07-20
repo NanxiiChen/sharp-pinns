549f0bbac80204d732e7d6e75d1838c50c511023
Nanxi Chen <nxchen@tongji.edu.cn>
2024-07-20 20:54:15 +0800

    change data

--- read_tensorboard.py
+++ read_tensorboard.py
@@ -1,7 +1,8 @@
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
 # 初始化一个EventAccumulator对象
-event_acc = EventAccumulator('../tf-logs/2024-06-16-14-19-57/events.out.tfevents.1718518797.autodl-container-ce3c4684af-54aea7eb')
+event_acc = EventAccumulator(
+    '../tf-logs/2024-06-16-14-19-57/events.out.tfevents.1718518797.autodl-container-ce3c4684af-54aea7eb')
 
 # 加载事件文件
 event_acc.Reload()
